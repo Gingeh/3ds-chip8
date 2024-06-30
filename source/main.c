fc2192cd0897663f0ea8cb615caa1dd9b9629d65
Gingeh <39150378+Gingeh@users.noreply.github.com>
2024-06-30 13:13:18 +1000

    init project

---
+++ source/main.c
@@ -0,0 +1,24 @@
+#include <3ds.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  gfxInitDefault();
+  consoleInit(GFX_TOP, NULL);
+
+  printf("Hello, world!\n");
+
+  // Main loop
+  while (aptMainLoop()) {
+    gspWaitForVBlank();
+    gfxSwapBuffers();
+    hidScanInput();
+
+    // Your code goes here
+    u32 kDown = hidKeysDown();
+    if (kDown & KEY_START)
+      break; // break in order to return to hbmenu
+  }
+
+  gfxExit();
+  return 0;
+}
